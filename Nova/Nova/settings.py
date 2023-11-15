fec1b40a0d9c78370c47cef2b9d3c26bc6af67a9
Наталья Арлазарова <sic15@yandex.ru>
2023-11-16 00:15:22 +0300

    1

--- Nova/Nova/settings.py
+++ Nova/Nova/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='0')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['62.84.123.59', 'task-nova.sytes.net', '127.0.0.1', 'localhost' ]
 
 
 # Application definition
